b4d86289aa195e055d1c9e96c20d74371c2d6827
paulocheque <paulocheque@gmail.com>
2015-04-05 02:13:27 -0300

    Fixed tornado handlers

--- codeart/benchmarks/_tornado.py
+++ codeart/benchmarks/_tornado.py
@@ -11,50 +11,50 @@ def version():
 
 
 class Request1kb(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         self.write(response1kb())
 
 class Request100kb(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         self.write(response100kb())
 
 class Request1mb(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         self.write(response1mb())
 
 class Request1s(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         self.write(responseSleep1s())
 
 class RequestJson(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_JSON)
         self.write(responseJson())
 
 class RequestHtml(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_HTML)
         self.write(responseHtml())
 
 
 class RequestDBcreate(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         mongoengine_create()
         self.write(OK)
 
 class RequestDBread(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         mongoengine_read()
         self.write(OK)
 
 class RequestDBcrud(tornado.web.RequestHandler):
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         mongoengine_crud()
         self.write(OK)
@@ -62,21 +62,21 @@ class RequestDBcrud(tornado.web.RequestHandler):
 
 class RequestDBcreateAsync(tornado.web.RequestHandler):
     @gen.coroutine
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         yield motorengine_create()
         self.write(OK)
 
 class RequestDBreadAsync(tornado.web.RequestHandler):
     @gen.coroutine
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         yield motorengine_read()
         self.write(OK)
 
 class RequestDBcrudAsync(tornado.web.RequestHandler):
     @gen.coroutine
-    def on_get(self, request, response):
+    def get(self):
         self.set_header('Content-Type', CONTENT_TYPE_PLAIN)
         yield motorengine_crud()
         self.write(OK)
