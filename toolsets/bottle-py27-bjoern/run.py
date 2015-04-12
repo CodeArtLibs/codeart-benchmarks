78305938fb1d2dcda45b239392cb93b17047bb02
paulocheque <paulocheque@gmail.com>
2015-04-12 05:01:40 -0300

    VirtualEnv defined inside of Foreman script

--- toolsets/bottle-py27-bjoern/run.py
+++ toolsets/bottle-py27-bjoern/run.py
@@ -1,4 +1,3 @@
-from bottle import run
 from codeart.benchmarks._bottle import *
 
 
@@ -6,4 +5,5 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get('PORT', 8000))
 
+    from bottle import run
     run(host='0.0.0.0', port=port, server='bjoern', quiet=True, reuse_port=True)
--- toolsets/bottle-pypy-gunicorn-1w/run.py
+++ toolsets/bottle-pypy-gunicorn-1w/run.py
@@ -1,11 +1,11 @@
-from bottle import run
 from codeart.benchmarks._bottle import *
 
-# import bottle
+import bottle
+app = bottle.default_app()
 
-# app = bottle.default_app()
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get('PORT', 8000))
-    run(host='0.0.0.0', port=port, server='gunicorn', quiet=True, workers=1)
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.environ.get('PORT', 8000))
+#     from bottle import run
+#     run(host='0.0.0.0', port=port, server='gunicorn', quiet=True, workers=1)
