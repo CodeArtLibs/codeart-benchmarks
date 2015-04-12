cfc53a49647d33a3b23c0f32942ad417d1b8667a
paulocheque <paulocheque@gmail.com>
2015-04-12 09:23:20 -0300

    Using waitress with Nginx

--- toolsets/bottle-pypy-waitress/run.py
+++ toolsets/bottle-pypy-waitress/run.py
@@ -2,18 +2,22 @@ from bottle import run
 from codeart.benchmarks._bottle import *
 
 
-if __name__ == "__main__":
-    import logging
-    logging.disable(logging.CRITICAL)
-    logger = logging.getLogger()
-    logger.setLevel(logging.CRITICAL)
-    logger.disabled = True
-    logger.propagate = False
-    logger = logging.getLogger('waitress')
-    logger.setLevel(logging.CRITICAL)
-    logger.disabled = True
-    logger.propagate = False
+import bottle
+app = bottle.default_app()
 
-    import os
-    port = int(os.environ.get('PORT', 8000))
-    run(host='0.0.0.0', port=port, server='waitress', quiet=True)
+
+# if __name__ == "__main__":
+#     import logging
+#     logging.disable(logging.CRITICAL)
+#     logger = logging.getLogger()
+#     logger.setLevel(logging.CRITICAL)
+#     logger.disabled = True
+#     logger.propagate = False
+#     logger = logging.getLogger('waitress')
+#     logger.setLevel(logging.CRITICAL)
+#     logger.disabled = True
+#     logger.propagate = False
+
+#     import os
+#     port = int(os.environ.get('PORT', 8000))
+#     run(host='0.0.0.0', port=port, server='waitress', quiet=True)
