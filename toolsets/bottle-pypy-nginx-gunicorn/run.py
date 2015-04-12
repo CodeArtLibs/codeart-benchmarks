9b964fdb1c937a14750f7d4797b86afde9be03b4
paulocheque <paulocheque@gmail.com>
2015-04-12 09:30:54 -0300

    Using multiple workers. ((2 * #cores/cpu) + 1)

---
+++ toolsets/bottle-pypy-nginx-gunicorn/run.py
@@ -0,0 +1,11 @@
+from codeart.benchmarks._bottle import *
+
+import bottle
+app = bottle.default_app()
+
+
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.environ.get('PORT', 8000))
+#     from bottle import run
+#     run(host='0.0.0.0', port=port, server='gunicorn', quiet=True, workers=5)
