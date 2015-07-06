622b229fa6bf480685fbb403502cbb3cfe23aebb
paulocheque <paulocheque@gmail.com>
2015-07-06 15:46:20 -0300

    wsgi common request code

---
+++ libs/python/codeart/benchmarks/_wsgi.py
@@ -0,0 +1,73 @@
+from codeart.benchmarks import *
+
+
+def response_1kb(environ, start_response):
+    data = RESPONSE_1KB
+    response_headers = [
+        ('Content-type', CONTENT_TYPE_PLAIN),
+        ('Content-Length', str(len(data))),
+    ]
+    start_response(b'200 OK', response_headers)
+    return [data]
+
+def response_100kb(environ, start_response):
+    data = RESPONSE_100KB
+    response_headers = [
+        ('Content-type', CONTENT_TYPE_PLAIN),
+        ('Content-Length', str(len(data))),
+    ]
+    start_response(b'200 OK', response_headers)
+    return [data]
+
+def response_1mb(environ, start_response):
+    data = RESPONSE_1MB
+    response_headers = [
+        ('Content-type', CONTENT_TYPE_PLAIN),
+        ('Content-Length', str(len(data))),
+    ]
+    start_response(b'200 OK', response_headers)
+    return [data]
+
+def response_json(environ, start_response):
+    response = RESPONSE_JSON
+    data = to_json(response)
+    response_headers = [
+        ('Content-type', CONTENT_TYPE_JSON),
+        ('Content-Length', str(len(data))),
+    ]
+    start_response(b'200 OK', response_headers)
+    return [data]
+
+def response_html(environ, start_response):
+    data = RESPONSE_HTML
+    response_headers = [
+        ('Content-type', CONTENT_TYPE_HTML),
+        ('Content-Length', str(len(data))),
+    ]
+    start_response(b'200 OK', response_headers)
+    return [data]
+
+def response_slow(environ, start_response):
+    data = responseSlow()
+    response_headers = [
+        ('Content-type', CONTENT_TYPE_PLAIN),
+        ('Content-Length', str(len(data))),
+    ]
+    start_response(b'200 OK', response_headers)
+    return [data]
+
+
+def app(environ, start_response):
+    path = environ['PATH_INFO']
+    if path.startswith('/json-response'):
+        return response_json(environ, start_response)
+    elif path.startswith('/1kb-response'):
+        return response_1kb(environ, start_response)
+    elif path.startswith('/100kb-response'):
+        return response_100kb(environ, start_response)
+    elif path.startswith('/1mb-response'):
+        return response_1mb(environ, start_response)
+    elif path.startswith('/slow-response'):
+        return response_slow(environ, start_response)
+    else:
+        return response_html(environ, start_response)
