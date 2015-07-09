b722947f90f16608b70855b8f31d6eeb7125d920
paulocheque <paulocheque@gmail.com>
2015-07-09 11:10:15 -0300

    Automatically try to connect to MongoDB using all engines. Fail silently.

--- libs/python/codeart/benchmarks/__init__.py
+++ libs/python/codeart/benchmarks/__init__.py
@@ -1,1 +1,18 @@
 from .responses import *
+
+
+try:
+    from mongoengine import connect
+    connect('default')
+    print('[MongoEngine] Connecting to DEV MongoDB')
+except Exception as e:
+    print('[MongoEngine] Unable to connect to MongoDB server')
+    print(str(e))
+
+try:
+    from motorengine import connect
+    connect('default')
+    print('[MotorEngine] Connecting to DEV MongoDB')
+except Exception as e:
+    print('[MotorEngine] Unable to connect to MongoDB server')
+    print(str(e))
